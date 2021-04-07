746ab0eb60c20a195012c8f70b56a435a160ebbd
emariec <79215468+emariec@users.noreply.github.com>
2021-04-07 13:22:52 +0200

    Tilføjet kommentarer

--- MinHeap.java
+++ MinHeap.java
@@ -4,12 +4,12 @@ import java.util.HashMap;
 public class MinHeap<T extends Comparable<T> >{
     HashMap<T,Integer> positionTable=new HashMap<>();
 
-    // root is at index 0
+    // Opret minheap som generic arraylist. Rodens position sættes til index 0.
     ArrayList<T> minheap;
     private int size;
     public MinHeap(){
         this.minheap=new ArrayList<T>();
-        this.size=0;
+        this.size=0; 
     }
     public int getPosition(T item){
         return positionTable.get(item);
@@ -18,37 +18,40 @@ public class MinHeap<T extends Comparable<T> >{
         return size <= 0;
     }
     private int Parent(int pos){
-        return (pos-1)/2;
+        return (pos-1)/2;//Fordi roden er på index 0, udregnes parent ved (index-1)/2
     }
     private int leftChild(int pos){
-        return pos*2 +1;
+        return pos*2 +1; // udregnes på denne måde fordi roden er på index 0
     }
     private int rightChild(int pos){
-        return pos*2 +2;
+        return pos*2 +2; // udregnes på denne måde fordi roden er på index 0
     }
-    private void swap(int pos1, int pos2){
-        T dummy= minheap.get(pos1);
+    private void swap(int pos1, int pos2){ //metode til at bytte elementer
+        T dummy= minheap.get(pos1); //midlertidig liste, hvor elementet på pos1 opbevares
 
-        minheap.set(pos1, minheap.get(pos2));
-        minheap.set(pos2,dummy);
-        positionTable.put(minheap.get(pos1),pos1);
+  
+        minheap.set(pos1, minheap.get(pos2)); //overwriter pos1 med element på pos2
+        minheap.set(pos2,dummy); // overwriter pos2 med element i dummy/ forrige pos1
+        positionTable.put(minheap.get(pos1),pos1); //opdaterer hashmap
         positionTable.put(minheap.get(pos2),pos2);
     }
+    
+    //metode til at insætte generic element T
     public void Insert(T item){
         minheap.add(item);
         positionTable.put(item,size);
-        size++;
-        decreasekey(size-1);
+        size++; //size øges når vi tilføjer et nyt element
+        decreasekey(size-1); // undersøg om nederste element i listen(det vi har tilføjet) skal rykkes op. 
     }
-    public void decreasekey(int pos){
+    public void decreasekey(int pos){ //metode til at rykke element op i heapen
         int currentpos=pos;
-        while (minheap.get(currentpos).compareTo(minheap.get(Parent(currentpos)))<0){
-            swap(currentpos,Parent(currentpos));
-            currentpos=Parent(currentpos);
+        while (minheap.get(currentpos).compareTo(minheap.get(Parent(currentpos)))<0){ //er det element vi ser på mindre end dens parent?
+            swap(currentpos,Parent(currentpos)); //bytter dem
+            currentpos=Parent(currentpos); //parents position er den nye currentpos, så vi kan tjekke parent til parent
         }
     }
 
-    public T viewMin(){
+    public T viewMin(){ //metode til at se mindste element, som altid vil befinde sig på index 0
         return minheap.get(0);
     }
     private boolean movedown(int pos){
@@ -58,7 +61,7 @@ public class MinHeap<T extends Comparable<T> >{
                 && (minheap.get(rightChild(pos)).compareTo(minheap.get(pos))<0);
         return leftsmaller || rightsmaller;
     }
-    public void increasekey(int pos){
+    public void increasekey(int pos){ 
         int currentpos=pos;
         while (movedown(currentpos))
         {
@@ -74,11 +77,11 @@ public class MinHeap<T extends Comparable<T> >{
             }
         }
     }
-    public T extractMin(){
-        T min = minheap.get(0);
-        minheap.set(0, minheap.get(size-1));
-        positionTable.put(minheap.get(0),0);
-        size--;
+    public T extractMin(){ //metode til at fjerne det mindste element fra listen
+        T min = minheap.get(0); //træk element på index 0 ud.
+        minheap.set(0, minheap.get(size-1)); //sæt listens sidste element ind på index 0. 
+        positionTable.put(minheap.get(0),0); //opdater hashmap
+        size--; //listens størrelse skal mindskes
         increasekey(0);
         return min;
     }
